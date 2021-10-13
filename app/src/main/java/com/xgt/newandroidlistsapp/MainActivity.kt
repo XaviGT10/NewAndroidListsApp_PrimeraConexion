0e644b77a9df8719289aedc845c7ce44d8ff00f7
Xavi Girón <xavier.giron@live.u-tad.com>
2021-10-13 10:36:36 +0200

    Primer commit salvado final clase2

--- app/src/main/java/com/xgt/newandroidlistsapp/MainActivity.kt
+++ app/src/main/java/com/xgt/newandroidlistsapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.xgt.newandroidlistsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvUser: RecyclerView = findViewById(R.id.rv_users)
+        val userOnclickListener = object : UserOnclickListener{
+            override fun onClick(user: String) {
+                Log.d("MainActivity", "User clicked from main activity: $user")
+            }
+        }
+        val adapter = User_adapter(retriveUsers()) {user ->
+            Log.d("MainActivity", "User clicked from main activity: $user")
+        }
+        rvUser.adapter = adapter
+        val layoutManager = LinearLayoutManager(this)
+        rvUser.layoutManager = layoutManager
+
+    }
+
+    private fun retriveUsers(): MutableList<String> {
+        return List(5) { index -> "Users from MAinActivity $index" }.toMutableList()
     }
 }
--- app/src/main/java/com/xgt/newandroidlistsapp/User_adapter.kt
+++ app/src/main/java/com/xgt/newandroidlistsapp/User_adapter.kt
@@ -1,10 +1,61 @@
 package com.xgt.newandroidlistsapp
 
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class User_adapter: RecyclerView.Adapter<User_adapter.UserViewHolder>() {
+class User_adapter(
+    private val users: MutableList<String>,
+    val onclickListener: (String) -> Unit
+) :
+    RecyclerView.Adapter<User_adapter.UserViewHolder>() {
 
-    class UserViewHolder(view: view:): RecyclerView.ViewHolder(view) {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view: View = layoutInflater.inflate(R.layout.item_user, parent, false)
+        return UserViewHolder(view)
     }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val username = users[position]
+
+        holder.tvUsername.text = username
+
+//        val onClickListener: View.OnClickListener = object  : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                onclickListener(username)
+//            }
+//        }
+
+//        holder.itemView.setOnClickListener(onClickListener)
+
+        holder.itemView.setOnClickListener {
+            onclickListener(username)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+    fun addUser(user: String){
+        users.add(user)
+        notifyItemInserted(users.size)
+    }
+
+    fun Usedeleter(user: String){
+        users.remove(user)
+        notifyDataSetChanged()
+    }
+
+    class UserViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val tvUsername: TextView = view.findViewById(R.id.tv_username)
+    }
+}
+
+interface UserOnclickListener {
+    fun onClick (user: String)
 }
