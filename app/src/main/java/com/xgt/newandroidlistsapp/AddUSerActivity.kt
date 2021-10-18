27123e590524b4695a40142827af627f6de3f1ad
Xavi Girón <xavier.giron@live.u-tad.com>
2021-10-18 13:49:24 +0200

    muchos cambios y un fallo aun

--- app/src/main/java/com/xgt/newandroidlistsapp/AddUSerActivity.kt
+++ app/src/main/java/com/xgt/newandroidlistsapp/AddUSerActivity.kt
@@ -1,23 +1,58 @@
 package com.xgt.newandroidlistsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
+import java.util.*
 
 class AddUSerActivity : AppCompatActivity() {
+    private lateinit var tietFirstName: TextInputEditText
+    private lateinit var tietLastName: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_user)
 
-        val firstNameNU: EditText = findViewById(R.id.et_Nombre2)
-        val nomUsr: EditText = findViewById(R.id.et_Usr)
+        val tietFirstName: TextInputEditText = findViewById(R.id.tiet_firstname)
+        val tietLastName: TextInputEditText = findViewById(R.id.tiet_lastname)
+
         val btnAdd: Button = findViewById(R.id.btn_addUsers)
+        val btnAdAndExit: Button = findViewById(R.id.btn_save_and_exit)
 
         btnAdd.setOnClickListener{
-            finish()
+            if (addUser()){
+                tietFirstName.text = null
+                tietLastName.text = null
+                tietFirstName.requestFocus()
+            }
+
+
+        }
+        btnAdAndExit.setOnClickListener{
+            if (addUser()){
+                finish()
+            }
+        }
+        btnAdAndExit.setOnClickListener { finish() }
+    }
+
+        private fun addUser(): Boolean{
+            val firstName = tietFirstName.text.toString()
+            val lastName = tietLastName.text.toString()
+
+            if (firstName.length > 3 && lastName.length > 3) {
+                val newUser = User(UUID.randomUUID().toString(), firstName, lastName)
+                (application as App).users.add(newUser)
+                return true
+            } else {
+                Toast.makeText(it.context, "Alguno de los campos no contiene el mínimo de caracteres requeridos (3", Toast.LENGTH_SHORT).show()
+            }
         }
     }
-}
+
--- app/src/main/java/com/xgt/newandroidlistsapp/MainActivity.kt
+++ app/src/main/java/com/xgt/newandroidlistsapp/MainActivity.kt
@@ -6,17 +6,25 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: User_adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rvUser: RecyclerView = findViewById(R.id.rv_users)
         val btnAddUser: Button = findViewById(R.id.btn_addUser)
+        val namePassed: String? = intent.extras?.getString("name")
+        val lastNamePassed: String? = intent.extras?.getString("last_name")
+        val tvFirstName: TextView = findViewById(R.id.tv_username)
+        val tvLastName: TextView = findViewById(R.id.tv_lastName)
+        tvFirstName.text = namePassed
+        tvLastName.text = lastNamePassed
         val userOnclickListener = object : UserOnclickListener{
             override fun onClick(user: String) {
                 Log.d("MainActivity", "User clicked from main activity: $user")
@@ -39,6 +47,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddUSerActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.notifyDataSetChanged()
     }
 
 //    private fun retriveUsers(): MutableList<User> {
