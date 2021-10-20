bc8203057549bcbe7e5e8a8b24a8ba65ffbe7735
Xavi Girón <xavier.giron@live.u-tad.com>
2021-10-20 10:43:37 +0200

    nuevos cambios y arreglos en new detail

--- app/src/main/java/com/xgt/newandroidlistsapp/NewDetailActivity.kt
+++ app/src/main/java/com/xgt/newandroidlistsapp/NewDetailActivity.kt
@@ -2,10 +2,12 @@ package com.xgt.newandroidlistsapp
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class NewDetailActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,24 +20,29 @@ class NewDetailActivity : AppCompatActivity()  {
             finish()
         }
 
-        var user: User? = null
-        (application as App).users.forEach { forUser ->
-            if (forUser.id == userId){
-                user = forUser
-                return
-            }
+//        var user: User? = null
+//        (application as App).users.forEach { forUser ->
+//            if (forUser.id == userId){
+//                user = forUser
+//                return@forEach
+//            }
 
-        }
+//        }
+
+        val user = (application as App).users.firstOrNull { forUser -> forUser.id == userId }
 
         if (user == null){
             //Show user that some error has taken place
+            Log.d("NewDetailUser", "user == null")
             finish()
         }
 
         val tvFirstName: TextView = findViewById(R.id.tv_nombreUser)
         val tvLastName: TextView = findViewById(R.id.tv_lastNameUsr)
+        val ivAvatar: ImageView = findViewById(R.id.iv_Avatar)
 
         tvFirstName.text = user?.firstName
         tvLastName.text = user?.lastName
+        Glide.with(this).load(user?.imageUrl).into(ivAvatar)
     }
 }
